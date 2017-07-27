ecf398df4bb34502f472f537da6e1c2a47d58769
Kevin Ye <kevin.ye141@gmail.com>
2017-07-27 14:55:05 -0400

    Added a scaffold for managing users, though it doesn't do anything yet

---
+++ Capstone.Web/Controllers/ManageUserController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Security.Cryptography;
+using Capstone.Web.Models.ViewModel;
+using Capstone.Web.Models.EntityManager;
+using Capstone.Web.Crypto;
+using Capstone.Web.Security;
+
+namespace Capstone.Web.Controllers
+{
+    public class ManageUserController : Controller
+    {
+        // GET: ManageUser
+        [AuthorizeRoles("Admin")]
+        public ActionResult ManageUsers()
+        {
+            return View();
+        }
+    }
+}

410472e19466b75a51f9735d721581382beb7206
Molly Fronius <froniusm@yahoo.com>
2017-08-01 15:14:17 -0400

    Current progress WaypointDAL

--- Capstone.Web/DAL/WaypointDAL.cs
+++ Capstone.Web/DAL/WaypointDAL.cs
@@ -12,6 +12,9 @@ namespace Capstone.Web.DAL
     {
         private string connectionString;
         private const string SQL_GetSchedules = "SELECT * FROM Schedules WHERE WaypointID = @waypointID;";
+        private const string SQL_CreateNewRoute = "INSERT INTO Routes VALUES(@Name, @Day, @IsPrivate);";
+        private const string SQL_CreateNewWaypoints= "INSERT INTO Waypoints VALUES(@RouteID, @Intersection, @Longitude, @Latitude);";
+        private const string SQL_GetRouteID = "SELECT RouteID FROM Routes WHERE Name = @Name;";
 
         public WaypointDAL(string connectionString)
         {
@@ -122,10 +125,79 @@ namespace Capstone.Web.DAL
 
         public void CreateNewRoute(Route r, List<Waypoint> waypoints)
         {
-            //Create the SQL commands
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
 
-            // Add to DB
+                    SqlCommand cmd = new SqlCommand(SQL_CreateNewRoute, conn);
+                    cmd.Parameters.AddWithValue("@Name", r.Name);
+                    cmd.Parameters.AddWithValue("@Day", r.Day);
+                    cmd.Parameters.AddWithValue("@IsPrivate", r.IsPrivate);
+
+                    cmd.ExecuteNonQuery();
+
+                    cmd = new SqlCommand(SQL_CreateNewWaypoints, conn);
+
+                    cmd.Parameters.AddWithValue("@RouteID", waypoints[0].RouteID);
+                }                   
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
+        public int GetRouteID(Route r)
+        {
+            string routeName = r.Name;
+            int routeID = -1;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(SQL_GetRouteID, conn);
+                    cmd.Parameters.AddWithValue("@RouteID", r.RouteID);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        routeID = Convert.ToInt32(reader["RouteID"]);
+                    }
+                    
+                }                 
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return routeID;
+        }
+
+        public Route PopulateRouteObject(SqlDataReader reader)
+        {
+            return new Route()
+            {
+                RouteID = Convert.ToInt32(reader["RouteID"]),
+                Name = Convert.ToString(reader["Name"]),
+                Day = Convert.ToString(reader["Day"]),
+                IsPrivate = Convert.ToBoolean(reader["IsPrivate"])
+            };
+        }
+
+        public Waypoint PopulateWaypointObject(SqlDataReader reader)
+        {
+            return new Waypoint()
+            {
+                WaypointID = Convert.ToInt32(reader["WaypointID"]),
+                RouteID = Convert.ToInt32(reader["RouteID"]),
+                Intersection = Convert.ToString(reader["Intersection"]),
+                Longitude = Convert.ToDecimal(reader["Longitude"]),
+                Latitude = Convert.ToDecimal(reader["Latitude"])
+            };
+        }
     }
 }
